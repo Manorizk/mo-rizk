9c6174b9df2576cddb7ec39437e4ac259a359728
mohamed rizk <134712817+Manorizk@users.noreply.github.com>
2024-06-10 16:48:59 +0300

    the diffrenece between for and while loop

---
+++ the diffrence between for loop & while l.py
@@ -0,0 +1,13 @@
+##the diffrence between for loop & while loop in paython 
+#for  needs an itreable object to itrate & used when number of iteration is 
+ 
+ for i in range (1,6)
+ print (i)
+
+ #while  executes based in some condtion & used when numbe rof iteration is not advanced
+ 
+ while true 
+    n=input("enter number :")
+    if n== q
+    break
+    print (n)
